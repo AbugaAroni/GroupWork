7d7a4f22e7896ce360ff13f1dc7286fec1ed239c
AbugaAroni <abugaaroni@gmail.com>
2020-08-05 20:26:54 +0300

    add cleanerer code for product objects with a constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,45 @@
 //Business interface logic
-/*
-function Products (pID, price, sdescription,description,pic,noProducs) {
-  this.ProductID= pID;
-  this.Cost= price;
-  this.shortDescription = sdescription;
-  this.longDescription = description;
+function Products (prodname, pid, cost, shortdescription,longdescription,pic,numProducts) {
+  this.pName= prodname;
+  this.pID = pid
+  this.price= cost;
+  this.sdescription = shortdescription;
+  this.description = longdescription;
   this.picture = pic;
-  this.quantity = noProducts;
+  this.quantity = numProducts;
 
 }
-*/
 
 //Products
-var p1 = {pName: "Diamond water", pID: 1, price: 200,sdescription: "Smooth freshening water that cools your thirst.",description: "Smooth freshening water that cools your thirst. Made from water that is said to have fallen from the heavens",pic: "../img/img1.jpg", noProducts: 10};
+var p1 = new Products("Diamond water", 1, 200, "Smooth freshening water that cools your thirst.", "Smooth freshening water that cools your thirst. Made from water that is said to have fallen from the heavens","../img/img1.jpg", 10);
 
-var p2 = {pName: "Vilsa Water", pID: 2, price: 250,sdescription: "Water from the Vilsa hills...", description: "From the hills of Vilsa, filtered by the hills and nature itself",pic: "../img/img2.jpg", noProducts: 10};
+var p2 = new Products("Vilsa Water", 2, 250, "Water from the Vilsa hills...", "From the hills of Vilsa, filtered by the hills and nature itself","../img/img2.jpg", 10);
 
-var p3 = {pName: "Mineral Water", pID: 3, price: 100,sdescription: "Fresh water with enriching minerals.",description: "Good old water, in a stylish bottle. Our most affordable product",pic: "../img/img3.jpg", noProducts: 10};
+var p3 = new Products("Mineral Water", 3, 100,"Fresh water with enriching minerals.","Good old water, in a stylish bottle. Our most affordable product", "../img/img3.jpg", 10);
 
-var p4 = {pName: "Acqua", pID: 4, price: 250, sdescription: "Natural spring water.", description: "Natural spring water, unfilited and tasty. Taste nature",pic: "../img/img4.jpg", noProducts: 15};
+var p4 = new Products("Acqua", 4, 250, "Natural spring water.", "Natural spring water, unfilited and tasty. Taste nature", "../img/img4.jpg", 15);
 
-var p5 = {pName: "Protein Water", pID: 5,price: 300, sdescription: "Has protein supplements good for health.",
-              description: "Created for gym bunnies, this will increase your gains 10fold",pic: "../img/img5.jpg", noProducts: 15};
+var p5 = new Products("Protein Water", 5,300,  "Has protein supplements good for health.",
+               "Created for gym bunnies, this will increase your gains 10fold","../img/img5.jpg", 15);
 
-var p6 = {pName: "Voss", pID: 6,price: 300, sdescription: "Voss water made of nature.", description: "Water from the famous Voss springs of Voss",pic: "../img/img6.jpg", noProducts: 12};
+var p6 = new Products("Voss", 6, 300, "Voss water made of nature.", "Water from the famous Voss springs of Voss","../img/img6.jpg", 12);
 
-var p7 = {pName: "Sun Bum", pID: 7,price: 250, sdescription: "Can't go wrong with sun bum in sunny days.", description: "Energizing drink that increases photothensis",pic: "../img/img7.jpg", noProducts: 20};
+var p7 = new Products("Sun Bum", 7, 250, "Can't go wrong with sun bum in sunny days.", "Energizing drink that increases photothensis","../img/img7.jpg", 20);
 
-var p8 = {pName: "Generosity", pID: 8, price: 200, sdescription: "Generosity water generous for training!", description: "Meant to be shared with friends! Increase training capabilities and team work sevenfold!",pic: "../img/img8.jpg", noProducts: 20};
+var p8 = new Products("Generosity",  8, 200, "Generosity water generous for training!", "Meant to be shared with friends! Increase training capabilities and team work sevenfold!", "../img/img8.jpg", 20);
 
-var products = {productID: [p1,p2,p3,p4,p5,p6,p7,p8]}
+var allproducts = {productID: [p1,p2,p3,p4,p5,p6,p7,p8]}
+
+
 
 //user interface logic
 $(document).ready(function() {
 
-    products.productID.forEach(function(product) {
-      console.log("lets go to " + product.pName);
-      console.log("lets go to " + product.price);
-      console.log("lets go to " + product.sdescription);
-    });
+  allproducts.productID.forEach(function(product) {
+     console.log(product.pName);
+     console.log(product.price);
+     console.log(product.sdescription);
+   });
 
 
     });
